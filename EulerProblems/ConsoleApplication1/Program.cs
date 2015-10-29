fed33f0da357bdbe508ec097f24f2d35f6bdce20
Michael Apfelbeck <m.apfelbeck@gmail.com>
2015-10-28 22:13:21 -0700

    Problem 13.

--- EulerProblems/ConsoleApplication1/Program.cs
+++ EulerProblems/ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var problem = new Problem12();
+            var problem = new Problem13();
 
             Console.WriteLine(problem.ProblemText+"\n");
 
